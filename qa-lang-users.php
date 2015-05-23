fea91d45afdd9c5c6432445d4b8c9290e05e2a55
minwook-shin <smw0106@naver.com>
2015-05-23 21:00:05 +0900

    Create qa-lang-users.php

---
+++ qa-lang-users.php
@@ -0,0 +1,144 @@
+<?php
+/*
+	Question2Answer by Gideon Greenspan and contributors
+	http://www.question2answer.org/
+
+	File: qa-include/qa-lang-users.php
+	Description: Language phrases for user management
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+	korea translation made by minwook shin
+	https://github.com/minwook-shin
+*/
+
+	return array(
+		'about' => '정보',
+		'add_user_x_favorites' => '나의 즐겨찾기로 ^ 유저 추가',
+		'approve_required' => '계정이 승인 될 때까지 기다리거나 ^1더 많은 정보를 추가^2 하십시오.',
+		'approve_title' => '유저 승인 대기중',
+		'approve_user_button' => '승인 유저',
+		'approved_user' => '승인된 유저들',
+		'avatar_default' => '기존',
+		'avatar_gravatar' => '나의 ^1Gravatar^2 보이기',
+		'avatar_label' => 'Avatar:',
+		'avatar_none' => '없음',
+		'block_user_button' => '제한 유저',
+		'blocked_users' => '제한된 유저들',
+		'category_level_add' => ' - ^1카테고리 추가-특정 권한^2',
+		'category_level_in' => '아래의 카테고리에 대한:',
+		'category_level_label' => '업그레이드됨:',
+		'category_level_none' => '업그레이드 안됨',
+		'change_email_link' => ' - ^1이메일 변경^2',
+		'change_password' => '비밀번호 변경',
+		'confirm_complete' => '감사합니다. - 당신의 이메일 주소는 확인되었습니다.',
+		'confirm_emailed' => '확인 링크를 이메일로 전송 되었습니다. 이메일 주소를 확인 할 수있는 링크를 클릭하십시오.',
+		'confirm_required' => '가입이 완료되었습니다, 이메일로 발송 된 확인 링크를 클릭, 혹은 ^1다시 요구^2.',
+		'confirm_title' => '이메일 주소 확인됨',
+		'confirm_wrong_log_in' => '코드가 정확하지않습니다. - ^1로그인^2 새로운 링크롤 보냅니다.',
+		'confirm_wrong_resend' => '코드가 정확하지않습니다. - 새로운 링크를 보내기위해 아래를 클릭하세요',
+		'delete_user_button' => '유저 삭제',
+		'edit_profile' => '나의 프로필 수정',
+		'edit_user_button' => '유저 수정',
+		'email_confirmed' => '확인됨',
+		'email_exists' => '이 계정의 이메일은 이미 사용되고있습니다.',
+		'email_handle_label' => '이메일 혹은 유저이름:',
+		'email_invalid' => '이메일이 다름 - 주의 깊게 확인하시기 바랍니다.',
+		'email_label' => '이메일:',
+		'email_not_confirmed' => '아직 확인되지않음',
+		'email_please_confirm' => '^5확인^6 해주십시오',
+		'email_required' => '이메일이 요구됨 - 공개되지않습니다.',
+		'forgot_link' => '나는 내 비밀번호를 잊어버렸습니다.',
+		'full_name' => '풀 네임',
+		'handle_empty' => '유저이름은 반드시 비어있지 말아야합니다.',
+		'handle_exists' => '유저 닉네임 중복 - 다른 시도하십시오',
+		'handle_has_bad' => '사용자 이름이 포함되지 않을 수 있습니다: ^',
+		'handle_label' => '유저이름:',
+		'hide_all_user_button' => '이 유저의 모든 게시물을 숨기기',
+		'last_login_label' => '마지막 로그인:',
+		'last_write_label' => '마지막 글쓴 행동:',
+		'level_admin' => '어드민',
+		'level_editor' => '수정자',
+		'level_expert' => 'Expert',
+		'level_for_category' => '^2 을 위한 ^1',
+		'level_in_general' => '대부분',
+		'level_moderator' => '게시판 관리자',
+		'level_super' => '슈퍼 관리자',
+		'location' => '지역',
+		'log_in_to_access' => '당신은 아마 지금 ^1로그인^2 당신의 계정을 제어하고있습니다.',
+		'login_button' => '로그인',
+		'login_limit' => '많은 로그인 시도 - 몇 시간후에 다시 시도하십시오',
+		'login_title' => '로그인',
+		'mass_mailings_explanation' => '모든 사용자에게 보내는 이메일을 구독',
+		'mass_mailings' => '대량의 메일:',
+		'member_for' => '맴버의:',
+		'member_type' => '타입:',
+		'new_password_1' => '새로운 비밀번호:',
+		'new_password_2' => '새 암호를 다시 입력:',
+		'no_blocked_users' => '제한 유저를 찾을수 없음',
+		'no_permission' => '이 작업을 수행 할 수있는 권한이 없습니다',
+		'old_password' => '기존 비밀번호:',
+		'only_shown_admins' => '(오직 어드민에게 보여짐)',
+		'only_shown_editors' => '(오직 수정자나 그 상위 권한까지 보여짐)',
+		'only_shown_experts' => '(오직 전문가나 그 상위 권한까지 보여짐)',
+		'only_shown_moderators' => '(오직 모더레이터나 어드민에게만 보여짐)',
+		'password_changed' => '비밀번호가 바뀜',
+		'password_label' => '비밀번호:',
+		'password_min' => '비밀번호는 적어도 ^ 글자이여야합니다.',
+		'password_mismatch' => '새로운 비밀번호가 매치되지않음.',
+		'password_none' => '없음. 직접 로그인하려면, 아래에 암호를 설정합니다.',
+		'password_sent' => '새 암호가 이메일로 전송 되었습니다',
+		'password_to_set' => '당신의 계정 페이지를 설정하십시오.',
+		'password_wrong' => '비밀번호가 맞지않음.',
+		'private_messages_explanation' => '이메일을 당신에게 보낼수있게 허용된 유저 (당신의 주소는 보지못함)',
+		'private_messages' => '비밀 메시지:',
+		'profile_saved' => '프로필 저장됨',
+		'wall_posts' => '담벼락 게시물:',
+		'wall_posts_explanation' => '당신의 담벼락에 게시물 게시를 허용한 유저 (당신에게 이메일로 전송됩니다)',
+		'register_button' => '가입',
+		'register_limit' => '너무 많은 가입시도 - 몇 시간후 다시 시도해주십시오',
+		'register_suspended' => '새로운 유저의 가입은 임시적으로 제한되었습니다. 곧 다시 시도해주십시오.',
+		'register_title' => '새로운 유저의 가입',
+		'registered_label' => '가입됨:',
+		'registered_user' => '유저가 가입됨',
+		'remember_label' => '이 컴퓨터를 기억',
+		'remember' => '기억',
+		'remove_avatar' => 'avatar 삭제:',
+		'reset_code_another' => '다른것을 보내다',
+		'reset_code_emailed' => '당신의 초기화 코드는 보내졌습니다.',
+		'reset_code_label' => '코드:',
+		'reset_code_wrong' => '코드가 일치하지않음',
+		'reset_title' => '잊어버린 비밀번호 초기화',
+		'save_profile' => '프로필 저장',
+		'save_user' => '유저 저장',
+		'send_confirm_button' => '확인 링크 보내기',
+		'send_password_button' => '새 비밀번호 보내기',
+		'send_reset_button' => '비밀번호 초기화 이메일 보내기',
+		'send_reset_note' => '메시지가 지시된 이메일 주소로 발송됩니다 .',
+		'special_users' => '스페셜 유저',
+		'terms_not_accepted' => 'You must tick this.',
+		'unblock_user_button' => '제한해제 유저',
+		'unsubscribe_complete' => '당신은 ^0로 부터 대량 발송되는 메일링을 구독취소했습니다. 당신의 ^1계정^2 페이지를 통해 다시 구독이 가능합니다.',
+		'unsubscribe_title' => '구독 취소',
+		'unsubscribe_wrong_log_in' => '코드가 일치하지 않음 - ^1로그인^2 하여 구독취소 하십시오.',
+		'unsubscribe' => '구독 취소:',
+		'user_blocked' => '(제한됨)',
+		'user_not_found' => '유저를 찾을수없음',
+		'website' => '웹사이트',
+		'x_ago_from_y' => '^1 전에 ^2 으로 접속',
+	);
+
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
