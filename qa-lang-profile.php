d78b0843249f3cc92e92cffe2461b7ac38d138ac
minwook-shin <smw0106@naver.com>
2015-05-23 20:59:00 +0900

    Create qa-lang-profile.php

---
+++ qa-lang-profile.php
@@ -0,0 +1,94 @@
+<?php
+/*
+	Question2Answer by Gideon Greenspan and contributors
+	http://www.question2answer.org/
+
+	File: qa-include/qa-lang-profile.php
+	Description: Language phrases for user profile page
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+	korea translation made by minwook shin
+	https://github.com/minwook-shin
+*/
+
+	return array(
+		'1_chosen_as_best' => ' (1 이 최고로 선정됨)',
+		'1_down_vote' => '1 down 투표',
+		'1_up_vote' => '1 up 투표',
+		'1_with_best_chosen' => ' (가장 좋은 대답 1이 선택됨)',
+		'activity_by_x' => '^으로부터 활성화됨',
+		'answers_by_x' => '^에 의해 답변',
+		'answers' => '답변:',
+		'bonus_points' => '보너스 포인트:',
+		'comments' => '댓글:',
+		'delete_wall_post_popup' => '이 담벼락 게시물 삭제',
+		'delete_pm_popup' => '이 비밀 메시지 삭제',
+		'extra_privileges' => '추가 권한:',
+		'gave_out' => '밖으로:',
+		'my_account_title' => '나의 계정',
+		'no_answers_by_x' => '^에 의한 답변이 없음',
+		'no_posts_by_x' => '^로 인한 게시물이 없음',
+		'no_questions_by_x' => '^로 부터 질문이 없음',
+		'permit_anon_view_ips' => '익명의 게시물의 ip 보기',
+		'permit_close_q' => '모든 질문 닫기',
+		'permit_delete_hidden' => '숨은 게시물 삭제',
+		'permit_edit_a' => '모든 답변 수정',
+		'permit_edit_c' => '모든 댓글 수정',
+		'permit_edit_q' => '모든 질문 수정',
+		'permit_edit_silent' => '조용히 게시물 수정',
+		'permit_flag' => '신고 게시물',
+		'permit_hide_show' => '모든 게시물 보이거나 숨기기',
+		'permit_moderate' => '게시물 을 승인 또는 거부',
+		'permit_post_a' => '질문에 대한 답변',
+		'permit_post_c' => '댓글 추가',
+		'permit_post_q' => '질문 추가',
+		'permit_post_wall' => '유저의 담벼락에 게시',
+		'permit_recat' => '모든 질문을 다시 분류',
+		'permit_retag' => '어떤 질문을 태그 재지정',
+		'permit_select_a' => '어떤 질문에 대한 답을 선택',
+		'permit_view_q_page' => '질문 페이지들 보기',
+		'permit_view_voters_flaggers' => '누가 투표했는지 보거나 게시물을 신고 지정',
+		'permit_vote_a' => '답변에 투표',
+		'permit_vote_down' => 'down게시물에 투표',
+		'permit_vote_q' => '질문에 투표',
+		'post_wall_blocked' => '이 사용자는 자신의 담벼락에 게시물을 게시를 비허용했습니다.',
+		'post_wall_button' => '담벼락 게시물 추가',
+		'post_wall_empty' => '이 담벼락에 게시 할 수있는 무언가를 입력하세요',
+		'post_wall_limit' => '이 시간에는 더이상 담벼락에 게시물을 쓸 수 없습니다',
+		'post_wall_must_be_approved' => '이 계정은 담벼락에 게시할려면 승인을 받아야합니다.',
+		'post_wall_must_confirm' => '담벼락에 게시하려면 ^5당신의 이메일 주소 확인^6 해야합니다.',
+		'post_wall_must_login' => '담벼락에 게시하려면 ^1로그인^2 하거나 ^3가입^4 해야합니다.',
+		'questions_by_x' => '^ 의해 질문',
+		'questions' => '질문들:',
+		'ranked_x' => ' (순위 #^)',
+		'received' => '수신:',
+		'recent_activity_by_x' => '^의 최근 활동',
+		'score' => '스코어:',
+		'send_private_message' => ' - ^1비밀 메시지 보내기^2',
+		'set_bonus_button' => '보너스 업데이트',
+		'title' => '주제:',
+		'user_x' => '유저 ^',
+		'voted_on' => '투표:',
+		'wall_for_x' => '^ 위한 담벼락',
+		'wall_view_more' => '더많은 담벼락 게시물 보기',
+		'x_chosen_as_best' => ' (^ 최고로 선정)',
+		'x_down_votes' => '^ down 투표',
+		'x_up_votes' => '^ up 투표',
+		'x_with_best_chosen' => ' (^ 선택한 가장 좋은 대답)',
+	);
+
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
